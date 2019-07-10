42247bcd803b69c692c8192281d6ee0b8de24401
akash <akash.aher@freshworks.com>
2019-07-10 18:03:35 +0530

    Initial commit

---
+++ app/src/main/java/example/com/firebaseapp/presentation/view/BaseActivity.kt
@@ -0,0 +1,10 @@
+package example.com.firebaseapp.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+
+/**
+ * Created by Akash on 2019-07-08.
+ */
+
+class BaseActivity : AppCompatActivity() {
+}
