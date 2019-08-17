252ceb6a94079b450238f029b8f6bad3375a0dc6
akash <akash.aher@freshworks.com>
2019-08-17 18:24:25 +0530

    added base structure
    Signed-off-by: akash <akash.aher@freshworks.com>

--- app/src/main/java/example/com/firebaseapp/presentation/view/BaseActivity.kt
+++ app/src/main/java/example/com/firebaseapp/presentation/view/BaseActivity.kt
@@ -1,11 +1,7 @@
 package example.com.firebaseapp.presentation.view
 
-import androidx.appcompat.app.AppCompatActivity
-
 /**
- * Created by Akash on 2019-07-08.
+ * Created by Akash on 2019-08-17.
  */
-
-open class BaseActivity : AppCompatActivity() {
-
+class BaseActivity {
 }
